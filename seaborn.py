fbb666043ddbc83c0797459354cd3d179fc1cfa1
yasinmillers <ssenyonga100@gmail.com>
2025-10-29 22:19:49 +0300

    continue

--- seaborn.py
+++ seaborn.py
@@ -1,1 +1,9 @@
+import seaborn as sns
 import matplotlib.pyplot as plt
+# Sample data
+x_values = [1, 2, 3, 4, 5]
+y_values = [2, 4, 6, 8, 10]
+
+# Creating a line plot using Seaborn
+sns.lineplot(x=x_values, y=y_values)
+plt.show()
