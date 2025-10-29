5b3dbd4f3941a793359f02f925684b01df797f60
yasinmillers <ssenyonga100@gmail.com>
2025-10-29 22:40:20 +0300

    nut

--- seaborn.py
+++ seaborn.py
@@ -1,9 +1,6 @@
-import seaborn as sns
 import matplotlib.pyplot as plt
-# Sample data
-x_values = [1, 2, 3, 4, 5,4,7,10]
-y_values = [2, 4, 6, 8, 10,12,14,16]
-
-# Creating a line plot using Seaborn
-
+plt.rcParams["figure.figsize"] = [8, 8]
+plt.rcParams["figure.autolayout"] = True
+x = [1, 2, 3, 1, 2, 3, 4, 1, 3, 4, 5]
+plt.hist(x, orientation="vertical")
 plt.show()
